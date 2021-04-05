ea85ee098e3b3d6df85b986d1ce794be26669fdf
glenpaulsonv <glenpaulsonv@gmail.com>
2021-04-05 10:39:09 +0530

    add_strings_using_stringBuilder.java
    solve without direct conversion from string to integer.

--- add_strings_using_stringBuilder.java
+++ add_strings_using_stringBuilder.java
@@ -4,11 +4,11 @@ class Solution {
         StringBuilder res = new StringBuilder();
             
         int carry=0;
-        int p1 = num1.length()-1;
-        int p2 = num2.length()-1;
+        int p1 = num1.length()-1; //pointer
+        int p2 = num2.length()-1; //pointer
         while(p1>=0 || p2>=0)
         {
-            int x1 = p1>=0 ? num1.charAt(p1) - '0' : 0;
+            int x1 = p1>=0 ? num1.charAt(p1) - '0' : 0; //num1.charAt(p1) - '0' -> Returns the actual integer value
             int x2 = p2>=0 ? num2.charAt(p2) - '0' : 0;
             int value = (x1+x2+carry)%10;
             carry = (x1+x2+carry)/10;
