a66a0d372de64afedff4e8636dcf518bf50d790d
glenpaulsonv <glenpaulsonv@gmail.com>
2021-04-13 21:12:28 +0530

    power_of_four.java

---
+++ power_of_four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        while(n!=4)
+        {
+            if(n%4!=0)
+                return false;
+            n=n/4;
+        }
+        return true;
+    }
+}
