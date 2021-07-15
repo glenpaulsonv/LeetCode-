2efb24c0d74c1bb71cb1fcdb9b16b1af5579b620
glenpaulsonv <glenpaulsonv@gmail.com>
2021-07-15 09:39:58 +0530

    single_number_solution_4

--- single_number.java
+++ single_number.java
@@ -52,3 +52,21 @@ class Solution {
 }
 
 //Solution 3 - using sets - took the help of leetcode solution
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> hash_table = new HashMap<>();
+        for(int i:nums)
+        {
+            hash_table.put(i, hash_table.getOrDefault(i,0)+1);
+        }
+        for(int i:nums)
+        {
+            if(hash_table.get(i)==1) return i;
+        }
+        return 0;
+    }
+}
+
+//Solution 4 - using HashTable - getOrDefault - took the help of LeetCode Solution
