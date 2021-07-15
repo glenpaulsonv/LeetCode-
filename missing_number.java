d7b66f716131058b099e5fa88ae3a8fda0f219c9
glenpaulsonv <glenpaulsonv@gmail.com>
2021-07-15 11:37:36 +0530

    2_sum.java

--- missing_number.java
+++ missing_number.java
@@ -24,3 +24,21 @@ class Solution {
 }
 
 //Solution 2 - using Bit Manipulation - took the help of leetcode solution
+
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums)
+        {
+            set.add(i);
+        }
+        for(int j=0;j<nums.length+1;j++)
+        {
+            if(!set.contains(j)) return j;
+        }
+        return -1;
+    }
+}
+
+//Solution 3 - using HashSet similar to Brute Force - took the help of the Leetcode solution
