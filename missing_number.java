0b21a48217b6db78ee0b88365543f8b11013148a
glenpaulsonv <glenpaulsonv@gmail.com>
2021-07-15 10:19:46 +0530

    missing_number_sol_2.java

--- missing_number.java
+++ missing_number.java
@@ -11,3 +11,16 @@ class Solution {
 }
 
 //Solution 1 - Using Gauss Formula for the sum of first n natural no.s - took the help of the leetcode solution
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int missingNo = nums.length;
+        for(int i=0;i<nums.length;i++)
+        {
+            missingNo = missingNo^i^nums[i];
+        }
+        return missingNo;
+    }
+}
+
+//Solution 2 - using Bit Manipulation - took the help of leetcode solution
