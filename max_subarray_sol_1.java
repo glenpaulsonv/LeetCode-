defa711f09a3f214e15a44d24a8c388ba00e5d7a
glenpaulsonv <glenpaulsonv@gmail.com>
2021-07-30 16:55:38 +0530

    max_subarray_sol_1.java

---
+++ max_subarray_sol_1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currSubarray = nums[0];
+        int maxSubarray = nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            int num = nums[i];
+            currSubarray = Math.max(num,currSubarray+num);
+            maxSubarray = Math.max(maxSubarray, currSubarray);
+        }
+        return maxSubarray;
+    }
+}
+
+//Solution 1 
+
+
